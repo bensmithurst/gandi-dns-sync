43124a9c2cc427e05fdf147709b39421213bd7e8
Ben Smithurst <github@bensmithurst.com>
2023-04-01 22:01:56 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# gandi-dns-sync
+
+This is a small Perl script to sync DNS settings from YAML files to Gandi's LiveDNS API.
---
+++ sync.pl
@@ -0,0 +1,377 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use English qw(-no_match_vars);
+use HTTP::Status qw(HTTP_NO_CONTENT HTTP_NOT_FOUND);
+use IO::Dir;
+use JSON;
+use LWP::UserAgent;
+use Readonly;
+use Socket;
+use Text::Diff;
+use YAML qw(LoadFile);
+
+Readonly my $API => 'https://api.gandi.net/v5';
+Readonly my $SHORT => 300;
+Readonly my $MEDIUM => 3600;
+Readonly my $LONG => 86400;
+
+my $KEY;
+my $ua;
+
+main();
+
+# Sync DNS entries for every domain with a domains/$fqdn.yaml file.
+sub main {
+	init();
+
+	my $dir = IO::Dir->new('domains') or die "cannot opendir domains: $ERRNO";
+	while (defined (my $file = $dir->read())) {
+		next unless $file =~ /(.*)\.yaml$/i;
+		my $fqdn = $1;
+
+		createDomain($fqdn);
+		syncDomain($fqdn);
+	}
+
+	return;
+}
+
+# Create domain in LiveDNS if it doesn't already exist.
+sub createDomain {
+	my ($fqdn) = @_;
+
+	my $existing = rawRequest('GET', "livedns/domains/$fqdn");
+	if ($existing->code == HTTP_NOT_FOUND) {
+		request('POST', 'livedns/domains', { fqdn => $fqdn });
+	} elsif (!$existing->is_success) {
+		die "Querying $fqdn failed: ".$existing->as_string();
+	}
+
+	return;
+}
+
+
+sub syncDomain {
+	my ($fqdn) = @_;
+
+	my $localData = loadZone($fqdn);
+
+	my $remoteData = loadRemoteZone($fqdn);
+
+	my @allKeys = (keys(%$localData), keys(%$remoteData));
+	my %allKeys = map { $_ => 1 } @allKeys;
+	my $changed;
+
+	foreach my $key (sort keys %allKeys) {
+		if ($localData->{$key} && $remoteData->{$key}) {
+			my $rr = $localData->{$key};
+			if (__rrsetsDiffer($rr, $remoteData->{$key})) {
+				#promptRequest('PUT', "livedns/domains/$fqdn/records/$rr->{rrset_name}/$rr->{rrset_type}", $rr);
+				$changed = 1;
+			}
+		} elsif ($localData->{$key}) {
+			#promptRequest('POST', "livedns/domains/$fqdn/records", $localData->{$key});
+			$changed = 1;
+		} else {
+			my $rr = $remoteData->{$key};
+			#promptRequest('DELETE', "livedns/domains/$fqdn/records/$rr->{rrset_name}/$rr->{rrset_type}");
+			$changed = 1;
+		}
+	}
+
+	if ($changed) {
+		my @local = sort(__flattenZone($localData));
+		my @remote = sort(__flattenZone($remoteData));
+		print diff(\@remote, \@local);
+
+		promptRequest('PUT', "livedns/domains/$fqdn/records", { items => [ values %$localData ] });
+		# if No, should allow making individual changes?
+
+		loadRemoteZone($fqdn) if $changed;
+	}
+
+	return;
+}
+
+sub loadZone {
+	my ($fqdn) = @_;
+
+	my %data;
+	loadFileRecursively($fqdn, "domains/$fqdn.yaml", \%data);
+
+	my $spf = delete $data{__spf};
+
+	if ($spf) {
+		addRecord($fqdn, \%data, '@', $LONG, 'TXT', join(' ', 'v=spf1', @$spf, '-all'));
+	}
+
+	return \%data;
+}
+
+sub loadFileRecursively {
+	my ($fqdn, $file, $zoneData, $subdomain, $depth) = @_;
+
+	$depth = 0 unless defined $depth;
+
+	die if $depth > 1;
+
+	my $data = LoadFile($file);
+
+	if ($data->{include}) {
+		foreach my $inc (@{$data->{include}}) {
+			loadFileRecursively($fqdn, $inc->{file}, $zoneData, $inc->{subdomain}, $depth + 1);
+		}
+
+	}
+
+	loadSPF($zoneData, $data, $fqdn);
+
+	if ($data->{records}) {
+		my @records;
+		foreach my $rr (@{$data->{records}}) {
+			$rr->{name} .= ".$subdomain" if $subdomain;
+
+			for (my $i = 0; defined $rr->{values}->[$i]; $i++) {
+				$rr->{values}->[$i] =~ s/\$domain\b/$fqdn/i;
+			}
+
+			if ($rr->{name} =~ /^(.*?)(\d+)\.\.(\d+)(.*)$/) {
+				my ($pre, $low, $high, $suf) = ($1, $2, $3, $4);
+				foreach my $n ($low .. $high) {
+					my %copy = %$rr;
+					$copy{name} = $pre.$n.$suf;
+					$copy{value} =~ s/\$/$n/;
+					push @records, \%copy;
+				}
+			} else {
+				push @records, $rr;
+			}
+		}
+		foreach my $rr (@records) {
+			my $values = [];
+			if ($rr->{value}) {
+				push @$values, $rr->{value};
+			} else {
+				push @$values, @{$rr->{values}};
+			}
+
+			addRecord($fqdn, $zoneData, $rr->{name}, $rr->{ttl}, $rr->{type}, $values);
+		}
+	}
+
+	return;
+}
+
+sub loadSPF {
+	my ($zoneData, $data, $domain) = @_;
+
+	return unless $data->{spf};
+
+	foreach my $entry (@{$data->{spf}}) {
+		if ($entry =~ /^ip4:(.*)/) {
+			my $ip = $1;
+
+			# crude IP check
+			unless ($ip =~ /^[\d\.]+$/) {
+				# assume hostname
+				$ip = __resolveName($ip, $domain);
+			}
+
+			push @{$zoneData->{__spf}}, "ip4:$ip";
+		} else {
+			push @{$zoneData->{__spf}}, $entry;
+		}
+	}
+
+	return;
+}
+
+sub addRecord {
+	my ($domain, $data, $name, $ttl, $type, $values) = @_;
+
+	my $rr = __rrset($name, $ttl, $type, $values, $domain);
+	my $key = $rr->{rrset_name}.'/'.$rr->{rrset_type};
+
+	if (my $existing = $data->{$key}) {
+		push @{$rr->{rrset_values}}, @{$existing->{rrset_values}};
+	}
+
+	$data->{$key} = $rr;
+
+	return;
+}
+
+sub loadRemoteZone {
+	my ($fqdn) = @_;
+
+	my $data = request('GET', "livedns/domains/$fqdn/records?per_page=1000");
+	my %data;
+
+	my $fh = IO::File->new("remote/$fqdn.txt", 'w') or die $!;
+
+	foreach my $rr (sort { ($a->{rrset_name} cmp $b->{rrset_name}) || ($a->{rrset_type} cmp $b->{rrset_type}) } @$data) {
+		$data{$rr->{rrset_name}.'/'.$rr->{rrset_type}} = $rr;
+
+		foreach my $val (sort @{$rr->{rrset_values}}) {
+			my $name = $rr->{rrset_name};
+			my $rr_fqdn;
+			if ($name eq '@') {
+				$rr_fqdn = "$fqdn.";
+			} else {
+				$rr_fqdn = "$name.$fqdn.";
+			}
+
+			printf $fh "%s %d IN %s %s\n", $rr_fqdn, $rr->{rrset_ttl}, $rr->{rrset_type}, $val;
+		}
+	}
+
+	$fh->close();
+
+	return \%data;
+}
+
+sub init {
+	$ua = LWP::UserAgent->new;
+
+	my $fh = IO::File->new("$ENV{HOME}/.gandi-api-key", 'r') or die $!;
+	chop($KEY = $fh->getline());
+
+	$| = 1;
+
+	return;
+}
+
+sub promptRequest {
+	my ($method, $path, $data) = @_;
+
+	print "--------------\n";
+	print "$method $path";
+	if (defined $data) {
+		my $encoded = encode_json($data);
+		$encoded = substr($encoded, 0, 200).'...' if length($encoded) > 200;
+		print " with data: $encoded";
+	}
+	print "\n--- Execute? [y/N] ";
+
+	my $answer = <STDIN>;
+	if ($answer && $answer =~ /^y/i) {
+		return request($method, $path, $data);
+	}
+
+	print "SKIPPED\n\n";
+	sleep 1;
+	return;
+}
+
+sub rawRequest {
+	my ($method, $path, $data) = @_;
+
+	my $request = HTTP::Request->new($method, "$API/$path");
+	$request->header('Authorization', "Apikey $KEY");
+	$request->content(encode_json($data)) if defined $data;
+
+	return $ua->request($request);
+}
+
+sub request {
+	my ($method, $path, $data) = @_;
+
+	my $response = rawRequest($method, $path, $data);
+
+	if (!$response->is_success) {
+		die "$method $path: ".$response->as_string();
+	}
+
+	if ($response->code == HTTP_NO_CONTENT) {
+		print "$method $path returned no data\n";
+		return undef;
+	}
+
+	my $decoded = decode_json($response->content);
+
+	return $decoded;
+}
+
+sub __rrsetsDiffer {
+	my ($localRR, $remoteRR) = @_;
+
+	return 1 if $localRR->{rrset_ttl} != $remoteRR->{rrset_ttl};
+
+	my @localVal = sort @{$localRR->{rrset_values}};
+	my @remoteVal = sort @{$remoteRR->{rrset_values}};
+
+	while (@localVal && @remoteVal) {
+		my $localVal = shift @localVal;
+		my $remoteVal = shift @remoteVal;
+
+		return 1 if $localVal ne $remoteVal;
+	}
+
+	return (scalar(@localVal) != scalar(@remoteVal));
+}
+
+sub __rrset {
+	my ($name, $ttl, $type, $values, $domain) = @_;
+
+	$values = [ $values ] if !ref $values;
+
+	if ($type eq 'TXT') {
+		$values = [ map { /^".*"$/ ? $_ : "\"$_\"" } @$values ];
+	} elsif ($type eq 'ALIAS') {
+		$type = 'A';
+		$values = [ map { __resolveName($_, $domain) } @$values ];
+	}
+
+	if (defined $ttl) {
+		if ($ttl eq 'SHORT') {
+			$ttl = $SHORT;
+		} elsif ($ttl eq 'MEDIUM') {
+			$ttl = $MEDIUM;
+		} elsif ($ttl eq 'LONG') {
+			$ttl = $LONG;
+		}
+	} else {
+		$ttl = $MEDIUM;
+	}
+
+	return {
+		rrset_name => $name,
+		rrset_ttl => $ttl,
+		rrset_type => $type,
+		rrset_values => $values,
+	};
+}
+
+my %ipForName;
+sub __resolveName {
+	my ($name, $domain) = @_;
+
+	$name .= ".$domain" unless $name =~ /\.$/;
+
+	if (!defined $ipForName{$name}) {
+		my $binIp = scalar gethostbyname($name) or die "Failed to resolve $name";
+		my $ip = inet_ntoa($binIp);
+		$ipForName{$name} = $ip;
+	}
+
+	return $ipForName{$name};
+}
+
+sub __flattenZone {
+	my ($data) = @_;
+
+	my @list;
+
+	foreach my $key (sort keys %$data) {
+		my $rr = $data->{$key};
+		foreach my $val (sort @{$rr->{rrset_values}}) {
+			push @list, "$rr->{rrset_name} $rr->{rrset_ttl} $rr->{rrset_type} $val\n";
+		}
+	}
+
+	return @list;
+}
