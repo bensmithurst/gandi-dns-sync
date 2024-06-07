b287115399e9ee498dd25ecc81bfd46266acafba
Ben Smithurst <github@bensmithurst.com>
2024-06-07 17:57:37 +0100

    allow ignoring certain ranges

--- opnsense.pl
+++ opnsense.pl
@@ -4,15 +4,19 @@ use strict;
 use warnings;
 
 use FindBin qw($Bin);
+use Net::IP;
+use YAML qw(LoadFile);
 
 require "$Bin/common.pl";
 
 init();
-main();
+exit(main());
 
-my (%domains, %domainRecords);
+my (%domains, %domainRecords, $config);
 
 sub main {
+	$config = LoadFile('/home/opnsense/config.yaml');
+
 	foreach my $row (@{request('GET', 'livedns/domains')}) {
 		$domains{$row->{fqdn}} = 1;
 	}
@@ -21,6 +25,7 @@ sub main {
 		importFile($file);
 	}
 
+	return 0;
 }
 
 sub importFile {
@@ -34,9 +39,15 @@ sub importFile {
 
 		my ($name, $type, $value) = ($1, $2, $3);
 
-		next if $seen{$name}{$type}++;
-
 		next unless $name =~ /\./;
+
+		if (uc($type) eq 'A') {
+			next if ignoreIPv4($value);
+		} elsif (uc($type) eq 'AAAA') {
+			next if ignoreIPv6($value);
+		}
+
+		next if $seen{$name}{$type}++;
 
 		createRecord($name, $type, $value);
 	}
@@ -100,4 +111,42 @@ sub recordHasChanged {
 	my $secondValues = join(',', sort @{$second->{rrset_values}});
 
 	return $firstValues ne $secondValues;
+}
+
+my ($__ignore);
+
+sub ignoreIPv4 {
+	my ($ip) = @_;
+	return __ignore($ip, 'ipv4');
+}
+
+sub ignoreIPv6 {
+	my ($ip) = @_;
+	return __ignore($ip, 'ipv6');
+}
+
+sub __ignore {
+	my ($ip, $field) = @_;
+
+	$ip = Net::IP->new($ip) or die $ip;
+
+	my $type = $ip->iptype;
+	return 1 if $type eq 'SHARED'; # CGNAT
+
+	if (!$__ignore->{$field}) {
+		$__ignore->{$field} = [];
+		foreach my $range (@{$config->{ignore}->{$field}}) {
+			push @{$__ignore->{$field}}, Net::IP->new($range) or die $range;
+		}
+	}
+
+	foreach my $range (@{$__ignore->{$field}}) {
+		my $result = $ip->overlaps($range);
+
+		die sprintf('%s / %s / %s', $ip->prefix, $range->prefix, $result) unless defined $result;
+
+		return 1 if $result != $IP_NO_OVERLAP;
+	}
+
+	return 0;
 }
