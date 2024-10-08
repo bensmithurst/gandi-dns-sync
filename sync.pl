75a57ff67beaa66f5debbce66f3612c1da194d23
Ben Smithurst <github@bensmithurst.com>
2024-10-08 23:01:17 +0100

    store into remote/*.txt again

--- sync.pl
+++ sync.pl
@@ -42,10 +42,12 @@ sub syncDomain {
 	my $localZone = $yaml->loadZone($fqdn);
 
 	my $remoteZone = $localZone->remote->loadZone($fqdn);
+	my $changed = 0;
 
 	foreach my $rr ($remoteZone->getRecords()) {
 		if (!$localZone->getRecord($rr->name, $rr->type) && !$localZone->isIgnored($rr->name, $rr->type)) {
 			$remoteZone->deleteRecord($rr);
+			$changed = 1;
 		}
 	}
 
@@ -54,10 +56,27 @@ sub syncDomain {
 
 		if (!$other) {
 			$remoteZone->createRecord($rr);
+			$changed = 1;
 		} elsif (!$other->equals($rr)) {
 			$remoteZone->updateRecord($rr, $other);
+			$changed = 1;
 		}
 	}
+
+	if ($changed) {
+		$remoteZone = $localZone->remote->loadZone($fqdn);
+	}
+
+	my @lines;
+	foreach my $rr (sort { $a->name cmp $b->name } $remoteZone->getRecords()) {
+		foreach my $value (@{$rr->values}) {
+			push @lines, sprintf("%s. %d IN %s %s\n",
+				$rr->getFQDN(), $rr->ttl, $rr->type, $value);
+		}
+	}
+	my $fh = IO::File->new("remote/$fqdn.txt", 'w') or die "Cannot write to remote/$fqdn.txt: $ERRNO";
+	$fh->print(@lines);
+	$fh->close();
 
 	return;
 }
